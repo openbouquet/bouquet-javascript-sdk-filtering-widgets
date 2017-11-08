abd3f925bed7ce49af0545f6caaea27a9ad273eb
Julien Theulier <julien@squidsolutions.com>
2017-11-08 11:30:48 +0100

    Missing dist update

--- dist/squid_api_filters-widget.js
+++ dist/squid_api_filters-widget.js
@@ -579,7 +579,7 @@ function program1(depth0,data) {
   buffer += "<div class=\"squid-api-range-selection-widget\">\n    <select class=\"form-control\">\n        <option value='custom'>Custom</option>\n        ";
   stack1 = helpers.each.call(depth0, (depth0 && depth0.ranges), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n    </select>\n    </ul>\n</div>";
+  buffer += "\n    </select>\n</div>\n<span class=\"caret\"></span>\n";
   return buffer;
   });
 
