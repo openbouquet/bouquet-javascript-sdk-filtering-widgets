706f42407773232ebf12d3d809927bcbb9ef69e2
Julien Theulier <julien@squidsolutions.com>
2017-11-08 14:09:36 +0100

    Put checkbox label before as it is a single checkbox choice

--- dist/squid_api_filters-widget.js
+++ dist/squid_api_filters-widget.js
@@ -637,7 +637,7 @@ function program6(depth0,data) {
 this["squid_api"]["template"]["squid_api_filters_segment_widget"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, helper, self=this, functionType="function", escapeExpression=this.escapeExpression;
+  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression, self=this;
 
 function program1(depth0,data) {
   
@@ -645,14 +645,14 @@ function program1(depth0,data) {
   return "checked=\"checked\"";
   }
 
-  buffer += "<div class=\"checkbox\">\r\n    <label>\r\n      <input type=\"checkbox\" ";
-  stack1 = helpers['if'].call(depth0, (depth0 && depth0.isSelected), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "> ";
+  buffer += "<div class=\"checkbox\">\r\n    <label>";
   if (helper = helpers.displayName) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.displayName); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "\r\n    </label>\r\n</div>";
+    + "</label>\r\n    <input type=\"checkbox\" ";
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.isSelected), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += ">\r\n</div>";
   return buffer;
   });
 
