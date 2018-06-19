662dcd2755d8f7949dbdab6cc989ecf5e8332723
Kostas Giannakakis <k.giannakakis@atc.gr>
2018-06-19 14:08:42 +0300

    Updating list after creating a selection

--- dist/squid_api_filters-widget.js
+++ dist/squid_api_filters-widget.js
@@ -3133,6 +3133,7 @@ $.widget( "ui.dialog", $.ui.dialog, {
             },
 
             "click #create-selection" : function(event) {
+                var me = this;
                 var projectId = this.config.get("project");
                 var bookmarkId = this.config.get("bookmark");
 
@@ -3151,17 +3152,49 @@ $.widget( "ui.dialog", $.ui.dialog, {
                     contentType: "text/json",
                     data: JSON.stringify(newSelection),
                     headers: {"Authorization" : "Bearer " + squid_api.model.login.get("accessToken")}
-                }).done(function(data) {
-                    console.log(data);
+                }).done(function(newSelection) {
+                    me.data.selections.push(newSelection);
+                    me.render();
                 });
             },
 
             "click .my-selection-name" : function(event) {
+                var me = this;
+                var projectId = this.config.get("project");
+                var bookmarkId = this.config.get("bookmark");
                 var myBookmarkSelectionId = $(event.target).parent().data("id");
 
-                var selection = $.grep(this.data.selections, function(elem) {
+                var mySelection = $.grep(this.data.selections, function(elem) {
                     return elem.id.myBookmarkSelectionId === myBookmarkSelectionId;}
                 )[0].selection;
+
+                console.log(this.data.selections[0]);
+                console.log(mySelection);
+
+                squid_api.model.config.attributes.selection = mySelection;
+
+                // get the Bookmark
+                // squid_api.getCustomer().then(function(customer) {
+                //     customer.get("projects").load(projectId).then(function(project) {
+                //         project.get("bookmarks").load(bookmarkId).done(function(bookmark) {
+                //             var forcedConfig = {};
+                //             var config = me.config.toJSON();
+                //             // exclude the selection from re-setting the config
+                //             for (var x in config) {
+                //                 if (x !== "selection") {
+                //                     forcedConfig[x] = config[x];
+                //                 }
+                //                 else {
+                //                     forcedConfig[x] = mySelection.selection;
+                //                 }
+                //             }
+                //             // set bookmark
+                //             squid_api.setBookmark(bookmark, forcedConfig);
+                //         }).fail(function(model, response, options) {
+                //             console.error("bookmark fetch failed : " + bookmarkId);
+                //         });
+                //     });
+                // });
 
                 this.close();
             },
